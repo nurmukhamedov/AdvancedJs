4fcc3a39736f919c0b0276debb9313b50ca45361
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2022-04-23 22:26:19 +0500

    added literal

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const myName = 'hello';
+const firstName = 'Muhammadrasul';
+const job = 'teacher';
+const birthYear = 2000;
+const year = 2022;
+const mRasul = "I'm " + firstName + ', a ' + job + ', a ' + (year - birthYear) + ' years old '; 
+console.log(mRasul);
 
-const myLastName = 'world';
+const newRasul = `I'm ${firstName} a ${job}, a ${year - birthYear} years old `;
+console.log(mRasul);
 
-console.log(`${myName} interesting ${myLastName}`);
